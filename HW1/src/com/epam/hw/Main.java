5ceb106cb109bc1ffeaa5d1271192c4aa311cf3f
toha04 <59120905+toha04@users.noreply.github.com>
2019-12-24 21:25:28 +0200

    Add files via upload

---
+++ HW1/src/com/epam/hw/Main.java
@@ -0,0 +1,12 @@
+package com.epam.hw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("* * * *    * * * *         *         *           *");
+        System.out.println("*          *     *       *   *       *  *     *  *");
+        System.out.println("* *        * * * *      *     *      *     *     *");
+        System.out.println("*          *           * * * * *     *           *");
+        System.out.println("* * * *    *          *         *    *           *");
+    }
+}
