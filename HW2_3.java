57aaf277fdeacad01853108556dc42d5158b90e5
toha04 <59120905+toha04@users.noreply.github.com>
2020-01-07 18:46:10 +0200

    Add files via upload
    added homework 2

---
+++ HW2_3.java
@@ -0,0 +1,19 @@
+package com.epam.java;
+import java.util.Scanner;
+
+public class HW2_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Invest your sum");
+        float invest = sc.nextFloat();
+        System.out.println("How long?");
+        int month = sc.nextInt();
+        System.out.println("Your deposit on 12 % / year");
+
+        float deposit = ((((12F /100F) / 12f) * invest) * month );
+        System.out.println("Your total sum will be : " + deposit);
+
+
+
+    }
+}
