68bdcce5eaccdfafe419cab5a32405f79f69553e
Anthony <anthony@Anthonys-MBP.lan>
2020-02-09 19:09:59 +0200

    HomeWork 7

---
+++ HW7/Rectangle.java
@@ -0,0 +1,23 @@
+package Java.HW7;
+
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+    public Rectangle(String color, double width, double height) {
+        super(color);
+        this.height = height;
+        this.width = height;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + " width=" + width + " height=" +height;
+    }
+    @Override
+    public double calcArea() {
+        if (width<=0||height<=0) {
+            return 0;
+        } else {
+            return width * height;
+        }
+    }
+}
